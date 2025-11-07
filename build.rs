c48331829e66be86221efe2ba5f759c783906bf9
Heath Stewart <heaths@outlook.com>
2025-11-06 23:42:06 -0800

    Add integration tests (#1)

---
+++ build.rs
@@ -0,0 +1,6 @@
+// Copyright 2025 Heath Stewart.
+// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+
+fn main() {
+    println!("cargo::rerun-if-changed=README.md");
+}
