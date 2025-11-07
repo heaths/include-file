5512653aeb8f10b7bec0d3b31fe6875be0106300
Heath Stewart <heaths@outlook.com>
2025-11-06 23:41:16 -0800

    Add integration tests

---
+++ tests/readme.rs
@@ -0,0 +1,22 @@
+// Copyright 2025 Heath Stewart.
+// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+
+use include_file::include_code;
+
+#[test]
+fn test_example() -> Result<(), Box<dyn std::error::Error>> {
+    include_code!("../README.md", "example");
+    Ok(())
+}
+
+#[derive(Debug)]
+struct Model {
+    #[allow(dead_code)]
+    name: String,
+}
+
+fn example() -> Result<Model, Box<dyn std::error::Error>> {
+    Ok(Model {
+        name: "example".into(),
+    })
+}
